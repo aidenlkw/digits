f573b2c5f7fec6372c167805f37926fddfb95dca
Aiden Woodard <aidenlkw@gmail.com>
2023-11-04 22:42:33 -1000

    Finished WOD in 40 minutes

---
+++ app/imports/ui/components/AddNote.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { AutoForm, ErrorsField, HiddenField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import swal from 'sweetalert';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import SimpleSchema from 'simpl-schema';
+import { Notes } from '../../api/note/Notes';
+
+// Create a schema to specify the structure of the data to appear in the form.
+const formSchema = new SimpleSchema({
+  note: String,
+  owner: String,
+  contactId: String,
+  createdAt: Date,
+});
+
+const bridge = new SimpleSchema2Bridge(formSchema);
+
+/* Renders the AddStuff page for adding a document. */
+const AddNote = ({ owner, contactId }) => {
+
+  // On submit, insert the data.
+  const submit = (data, formRef) => {
+    const { note, createdAt } = data;
+    Notes.collection.insert(
+      { note, contactId, createdAt, owner },
+      (error) => {
+        if (error) {
+          swal('Error', error.message, 'error');
+        } else {
+          swal('Success', 'Item added successfully', 'success');
+          formRef.reset();
+        }
+      },
+    );
+  };
+
+  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
+  let fRef = null;
+  return (
+    <Container className="py-3">
+      <Row className="justify-content-center">
+        <Col xs={10}>
+          <Col className="text-center"><h4>Add Timestamped Note</h4></Col>
+          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)}>
+            <Card>
+              <Card.Body>
+                <TextField name="note" />
+                <SubmitField value="Submit" />
+                <ErrorsField />
+                <HiddenField name="owner" value={owner} />
+                <HiddenField name="contactId" value={contactId} />
+                <HiddenField name="createdAt" value={new Date()} />
+              </Card.Body>
+            </Card>
+          </AutoForm>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+AddNote.propTypes = {
+  owner: PropTypes.string.isRequired,
+  contactId: PropTypes.string.isRequired,
+};
+
+export default AddNote;
